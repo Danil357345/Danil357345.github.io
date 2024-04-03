535f383fcf732985b8be0b099025d02a4f8c80a3
Danil357345 <128617132+Danil357345@users.noreply.github.com>
2024-04-03 18:12:00 +0300

    Create fizzbuzz.rs

---
+++ fizzbuzz/fizzbuzz.rs
@@ -0,0 +1,17 @@
+
+fn fizzbuzz(n: u32) {
+  for i in 1..n + 1 {
+    let output = match i {
+      x if x % 15 == 0 => "FizzBuzz",
+      x if x % 3 == 0 => "Fizz",
+      x if x % 5 == 0 => "Buzz",
+      _ => i,
+    };
+ 
+    println!("{}", output);
+  }
+}
+ 
+fn main() {
+  fizzbuzz(100);
+}
